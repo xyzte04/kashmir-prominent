daec12e3516d1ef97f5610317439aaf8c54cdd7b
SS <xyzte04@gmail.com>
2021-03-04 13:26:48 +0530

    add build

--- src/components/VoiceOver.js
+++ src/components/VoiceOver.js
@@ -19,7 +19,6 @@ const VoiceOver = ({
 
   // effect for initial setup
   useEffect(() => {
-    console.log(name, "bleh1");
     wavesurferRef.current = WaveSurfer.create({
       container: waveformRef.current,
       progressColor: "#dc6d6d",
@@ -42,10 +41,8 @@ const VoiceOver = ({
 
   // effect for loading file when user scrolls to it.
   useEffect(() => {
-    console.log(name, "bleh2");
     if (wavesurferRef && wavesurferRef.current) {
-      if (thisPersonIsActive && !loaded) {
-        console.log(name, "bleh23");
+      if (window.activePerson === name && !loaded) {
         wavesurferRef.current.load(audioFile);
 
         wavesurferRef.current.on("ready", function () {
@@ -61,11 +58,12 @@ const VoiceOver = ({
   }, [audioFile, thisPersonIsActive, loaded]);
 
   useEffect(() => {
-    console.log(name, "bleh3");
     if (wavesurferRef && wavesurferRef.current) {
-      if (thisPersonIsActive && loaded) {
+      if (window.activePerson === name && loaded) {
         window.activeAudio = wavesurferRef.current;
         wavesurferRef.current.play();
+      } else if (window.activePerson !== name && loaded) {
+        wavesurferRef.current.pause();
       }
     }
   }, [thisPersonIsActive, loaded]);
