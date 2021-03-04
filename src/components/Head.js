66f56cd0e8f0513d9a5c72dea99281c22e57d16e
SS <xyzte04@gmail.com>
2021-03-04 13:20:31 +0530

    new build

--- src/components/Head.js
+++ src/components/Head.js
@@ -9,11 +9,16 @@ const Head = ({ toggleLanguage }) => {
     <div id="head">
       <h1 className="big-serif">Faces of Kashmir</h1>
       <p className="subhed sans light">
-        In the past 30 years, terrorists have killed thousands of people.
+        In the past 30 years, terrorists have killed thousands of Kashmiris.
         <br></br> Here we talk about the 15 of those whose stories still remain
         in the public psyche.
       </p>
-      <div id="unmute-phone" onClick={() => setAudioOn(!audioOn)}>
+      <div
+        id="unmute-phone"
+        onClick={() => {
+          setAudioOn(!audioOn);
+        }}
+      >
         <img
           src={"images/" + (audioOn ? "sound-on.svg" : "sound-off.svg")}
           alt={audioOn ? "audio-on" : "audio-off"}
